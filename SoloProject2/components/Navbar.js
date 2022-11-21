a605fae3c905de264ff93c05d2e0edce2b0de9ed
Bao Lam <101058430+tbaolam@users.noreply.github.com>
2022-11-20 18:31:23 -0800

    Add files via upload

---
+++ SoloProject2/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src="../images/earth-logo.png" className="nav--logo" />
+            <p>My Travel Journal</p>
+        </nav>
+    )
+}
